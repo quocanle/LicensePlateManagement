f25ebdf26de2f1099ddc89ac83da25043fd36131
phoherozombie <151513205+phoherozombie@users.noreply.github.com>
2023-12-01 22:16:55 +0700

    Update AfterLogin.java

--- src/view/AfterLogin.java
+++ src/view/AfterLogin.java
@@ -23,6 +23,7 @@ public class AfterLogin extends javax.swing.JFrame {
         initComponents();
         setIcon();
         helloPrint();
+        this.setlocationrelativeto(null);
     }
 
     AfterLogin(CongAn ongCan) {
